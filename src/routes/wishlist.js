094c2f5fa30cdc047675b3db5c2a7867c3c53e1f
Lovesaini9824 <160708264+Lovesaini9824@users.noreply.github.com>
2026-01-24 16:03:35 +0530

    Update wishlist.js

--- src/routes/wishlist.js
+++ src/routes/wishlist.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {protect} = require('../middleware/auth');
-const { toggleWishlist, getWishlist,} = require('../controllers/wishlistController');
+const {
+  toggleWishlist,
+  getWishlist,
+} = require('../controllers/wishlistController');
 
 router.post('/toggle', protect, toggleWishlist);
 router.get('/get-wishlist', protect, getWishlist);
